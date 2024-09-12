a94f7aaf97dd33d09367ef91dfd07250fa3b6dbb
sidiq20 <sidiqolasode@gmail.com>
2024-09-12 11:53:19 +0100

    add new items faeture

--- web.py
+++ web.py
@@ -3,6 +3,12 @@ import functions
 
 todos = functions.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity")
@@ -11,4 +17,6 @@ st.write("This app is to increase your productivity")
 for index, todo in enumerate(todos):
     st.checkbox(todo, key=f"todo_{index}")
 
-st.text_input(label="Enter a todo:", placeholder="Add new todo....")
+st.text_input(label="", placeholder="Add new todo....", on_change=add_todo, key='new_todo')#
+
+st.session_state
