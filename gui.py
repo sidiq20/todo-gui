ad8017d9a87878ce8ce1f3b39ab51d7c7df947ad
sidiq20 <sidiqolasode@gmail.com>
2024-09-12 11:19:00 +0100

    create a static web interface

--- gui.py
+++ gui.py
@@ -1,24 +1,46 @@
 import functions
 import FreeSimpleGUI as sg
 import time
+import os
+import sys
+
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+# Create a todos.txt file if it does not exist
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", "w") as file:
+        pass
 
 sg.theme("Black")
 
+# GUI Elements
 clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add", size=10)
-list_box = sg.Listbox(values=functions.get_todos(), key='todos',
-                      enable_events=True, size=[45, 10])
+
+# Updated to use resource_path function for image paths
+add_button = sg.Button(size=(20, 1), image_source=resource_path("add.png"), mouseover_colors="LightBlue2",
+                       tooltip="Add todo", key="Add")
+list_box = sg.Listbox(values=functions.get_todos(), key='todos', enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
-complete_button = sg.Button("Complete")
+complete_button = sg.Button(image_source=resource_path("complete.PNG"), size=(20, 1), mouseover_colors="LightBlue2",
+                            key="Complete")
 exit_button = sg.Button("Exit")
 
-button_labels = ["Close", "Apply"]
-
+# Window Layout
 window = sg.Window('To-Do App',
                    layout=[[clock],
-                       [label],
+                           [label],
                            [input_box, add_button],
                            [list_box, edit_button, complete_button],
                            [exit_button]],
@@ -27,6 +49,7 @@ window = sg.Window('To-Do App',
 while True:
     event, values = window.read(timeout=200)
     window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -58,7 +81,11 @@ while True:
         case "Exit":
             break
         case 'todos':
-            window['todo'].update(value=values['todos'][0])
+            try:
+                window['todo'].update(value=values['todos'][0])
+            except IndexError:
+                window['output'].update(value="Please add a todo first!")
+                sg.popup("Please add a todo first!", font='Helvetica')
         case sg.WIN_CLOSED:
             break
 
